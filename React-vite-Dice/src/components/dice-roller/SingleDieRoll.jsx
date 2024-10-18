f7e10e479fc55c1d9394520401970fa0ed116b90
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-10-17 21:28:14 -0400

    Worked in Single Die Roll. issue with quantity, used number as a placeholder value

--- React-vite-Dice/src/components/dice-roller/SingleDieRoll.jsx
+++ React-vite-Dice/src/components/dice-roller/SingleDieRoll.jsx
@@ -4,28 +4,32 @@ import "./dice-roller.css";
 
 /* TODO make it so the die size is passed as props */
 /* TODO Number of dice should be pulled from form in this component */
-const SingleDieRoll = ({ diceSize }) => {
-  const [quantity, setQuantity] = useState();
+
+
+const SingleDieRoll = ({ diceSize }, number) => {
+const [quantity, setQuantity] = useState();
 
 const getQuantity = (e) => {
-console.log("e.target.value: ",e.target.value);
-setQuantity(e.target.value); // sets the value to match the event's input field value "e" and saves it as it changes
-console.log("Quantity: ",quantity);
+console.log("e.target.value: ",e.target.value, typeof(e.target.value));
+setQuantity(parseInt(e.target.value)); // sets the value to match the event's input field value "e" and saves it as it changes
+console.log("Quantity: ",quantity, typeof(quantity));
+number = quantity;
 }
 
+/* If I pass number into the following function instead of quanitty it gets to the alert but reads as undefined, otherwise it never gets there */
   function rollDice({ dieSize }, quantity) {
-    console.log(`size`, diceSize , `quantity`, quantity);
+    console.log(`size`, diceSize , `quantity`, quantity,`number`, number,);
     if (quantity.value > 0) {
       timesRolled += 1;
       console.log("Times Rolled: ", timesRolled);
       result.innerHTML += `<p class="rollCounter">Roll #${timesRolled}</p>`;
-      console.log(`d${size}Button clicked`);
-      console.log(`Count d${size}Count`);
-      rollDiceMultiple(size, quantity);
+      console.log(`d${dieSize}Button clicked`);
+      console.log(`Count d${dieSize}Count`);
+      rollDiceMultiple(dieSize, quantity);
       result.innerHTML += `<p class="sumOfDice">Sum of Dice = ${sumOfDice}</p>`;
       sumOfDice = 0;
     } else {
-      alert(`Select how many D${size}s you wish to roll.`);
+      alert(`Select how many D${dieSize}s you wish to roll.`);
     }
   }
   return (
