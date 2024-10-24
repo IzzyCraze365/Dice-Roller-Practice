2292d7457b86722828cfbc4885c26ee30059af83
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-10-24 15:55:44 -0400

    Single Dice Roller- There was a typo, now the diceSize is being read properly

--- React-vite-Dice/src/components/dice-roller/SingleDieRoll.jsx
+++ React-vite-Dice/src/components/dice-roller/SingleDieRoll.jsx
@@ -6,36 +6,39 @@ import "./dice-roller.css";
 /* TODO Number of dice should be pulled from form in this component */
 
 
-const SingleDieRoll = ({ diceSize }, number) => {
+const SingleDieRoll = ({ diceSize }) => {
+console.log("diceSize", diceSize, typeof(diceSize));
 const [quantity, setQuantity] = useState();
 
 const getQuantity = (e) => {
 console.log("e.target.value: ",e.target.value, typeof(e.target.value));
 setQuantity(parseInt(e.target.value)); // sets the value to match the event's input field value "e" and saves it as it changes
 console.log("Quantity: ",quantity, typeof(quantity));
-number = quantity;
 }
 
 /* If I pass number into the following function instead of quanitty it gets to the alert but reads as undefined, otherwise it never gets there, still testing, why isn't it working?? */
-  function rollDice({ dieSize }, quantity) {
-    console.log(`size`, diceSize , `quantity`, quantity,`number`, number);
-    if (quantity.value > 0) {
+  function rollDice() {
+
+    console.log(`size`, diceSize , `quantity`, quantity); //! TEST
+    if (quantity > 0) {
       timesRolled += 1;
       console.log("Times Rolled: ", timesRolled);
       result.innerHTML += `<p class="rollCounter">Roll #${timesRolled}</p>`;
-      console.log(`d${dieSize}Button clicked`);
-      console.log(`Count d${dieSize}Count`);
-      rollDiceMultiple(dieSize, quantity);
+      console.log(`d${diceSize}Button clicked`);
+      console.log(`Count d${diceSize}Count`);
+      rollDiceMultiple(diceSize, quantity);
       result.innerHTML += `<p class="sumOfDice">Sum of Dice = ${sumOfDice}</p>`;
       sumOfDice = 0;
     } else {
-      alert(`Select how many D${dieSize}s you wish to roll.`);
+      console.log(diceSize);
+      console.log(`Select how many D${diceSize}s you wish to roll.`);
+      alert(`Select how many D${diceSize}s you wish to roll.`);
     }
   }
   return (
     <>
       <div>SingleDieRoll</div>
-      <input onChange={getQuantity} type="number"></input>
+      <input onChange={getQuantity} type="number" min="0"></input>
       <button onClick={rollDice} className="button">
         Roll D{diceSize}
       </button>
