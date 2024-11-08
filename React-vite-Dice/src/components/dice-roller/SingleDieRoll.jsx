c48ecf5d6dac3d7e381be234e21b0ded5b53b2d7
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-11-08 12:30:43 -0500

    Single Dice Roller - notes about setState

--- React-vite-Dice/src/components/dice-roller/SingleDieRoll.jsx
+++ React-vite-Dice/src/components/dice-roller/SingleDieRoll.jsx
@@ -4,10 +4,10 @@ import "./dice-roller.css";
 import { DiceContext } from "./Context/DiceContext";
 import { ResultsContext } from "./Context/ResultsContext";
 
-
+// TODO okay now its time to look into setState
 
 /* The die size is passed as props */
-/* The Number of dice should be pulled from form in this component */
+/* The Number of dice should be pulled from from in this component */
 
 const SingleDieRoll = ({ diceSize }) => {
   const [timesRolled, setTimesRolled] = useContext(DiceContext); //This is pulling the values from the DiceContext Provider
@@ -75,106 +75,6 @@ function randomNum(min, max) {
   return Math.floor(Math.random() * range) + min;
 }
 
-//Todo the following funtions need to be editied
-/* 
-// Function to Roll all selected Dice
-function rollAllDice() {
-  let totalDice =
-    parseInt(d4Count.value) +
-    parseInt(d6Count.value) +
-    parseInt(d8Count.value) +
-    parseInt(d10Count.value) +
-    parseInt(d12Count.value) +
-    parseInt(d20Count.value) +
-    parseInt(d100Count.value) +
-    parseInt(dXCount.value);
-  console.log(`Total Dice being rolled =`, totalDice);
-  if (parseInt(dieSize.value) === 0 && parseInt(dXCount.value) > 0) {
-    console.log(
-      "dieSize.value",
-      parseInt(dieSize.value),
-      "dXCount.value",
-      parseInt(dXCount.value)
-    );
-    alert("Select number of sides on your dice.");
-    return;
-  } else if (totalDice > 0) {
-    timesRolled += 1;
-    console.log("Rolling all Dice, Times Rolled: ", timesRolled);
-    result.innerHTML += `<p class="rollCounter">Roll #${timesRolled}</p>`;
-    if (d4Count.value) {
-      // Do not need to specify that it is Greater than 0 because the if statement automatically checks to see that it has value.
-      rollDiceMultiple(4, d4Count);
-    }
-    if (d6Count.value) {
-      // checks to see .value > 0
-      rollDiceMultiple(6, d6Count);
-    }
-    if (d8Count.value) {
-      // checks to see .value > 0
-      rollDiceMultiple(8, d8Count);
-    }
-    if (d10Count.value) {
-      // checks to see .value > 0
-      rollDiceMultiple(10, d10Count);
-    }
-    if (d12Count.value) {
-      // checks to see .value > 0
-      rollDiceMultiple(12, d12Count);
-    }
-    if (d20Count.value) {
-      // checks to see .value > 0
-      rollDiceMultiple(20, d20Count);
-    }
-    if (d100Count.value) {
-      // checks to see .value > 0
-      rollDiceMultiple(100, d100Count);
-    }
-    if (dXCount.value) {
-      // checks to see .value > 0
-      for (let i = 0; i < dXCount.value; i++) {
-        console.log(`Rolling Die #${i + 1}`);
-        let outcome = randomNum(1, dieSize.value);
-        console.log(`Outcome: `, outcome);
-        result.innerHTML += `<p>Variable Die with ${dieSize.value} sides rolled a ${outcome}</p>`;
-        sumOfDice += outcome;
-      }
-    }
-    result.innerHTML += `<p class="sumOfDice">Sum of Dice = ${sumOfDice}</p>`;
-    sumOfDice = 0;
-  } else {
-    alert("Select some number of Dice to roll.");
-  }
-}
-
-
-
-
-// Resets all Values for the Dice Rollers
-function reset() {
-  console.log(`Reset Button Clicked`);
-  clearHistory();
-  d4Count.value = 0;
-  d6Count.value = 0;
-  d8Count.value = 0;
-  d10Count.value = 0;
-  d12Count.value = 0;
-  d20Count.value = 0;
-  d100Count.value = 0;
-  dXCount.value = 0;
-  dieSize.value = 0;
-}
-
-// Clears the History of the Dice Roller but preserves the numbers in the Code.
-function clearHistory() {
-  console.log(`Clearing History`);
-  timesRolled = 0;
-  sumOfDice = 0;
-  result.textContent = "";
-  display.style.display = "none";
-  clearHistoryButton.style.display = "none";
-}
- */
 
   return (
     <>
